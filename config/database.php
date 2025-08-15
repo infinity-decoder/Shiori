7e6752362cd1531aa9b5206a12c8cfeb617ddfaf
infinity-decoder <bc210427835mal@vu.edu.pk>
2025-08-15 09:19:37 +0500

    modified:   config/database.php

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host'    => '127.0.0.1',
+    'port'    => 3306,
+    'name'    => 'FGSS',
+    'user'    => 'root',   // change if you use a different DB user
+    'pass'    => '',       // change if you set a password
+    'charset' => 'utf8mb4',
+];
