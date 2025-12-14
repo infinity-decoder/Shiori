011bcef19c3b7547941c6ae5a1e744c8ac03fa60
infinity-decoder <bc210427835mal@vu.edu.pk>
2025-12-14 19:13:14 +0500

    deleted:    config/database.php

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host'    => 'localhost',
-    'port'    => 3306,
-    'name'    => 'shiori_db',
-    'user'    => 'root',
-    'pass'    => '',
-    'charset' => 'utf8mb4',
-];
